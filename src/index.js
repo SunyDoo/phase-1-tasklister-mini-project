ee6f5d980f051a3243591767246f8d84c4405ccc
Sunil <snilavarath@gmail.com>
2021-12-10 11:01:49 -0500

    pushing code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+  let btn = document.getElementById('btn')
+  btn.addEventListener('click', handleDelete)
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    buildTaskList(e.target.newtaskdescription.value)
+    form.reset()
+      })
 });
+
+function buildTaskList(description){
+  let option = document.createElement('option')
+  option.textContent = `${description} `
+  document.querySelector('#tasks').appendChild(option)
+  console.log(document.querySelector('option'))
+}
+
+
+
+function handleDelete(e){ 
+  console.log(option[0])
+  e.option[0].remove()
+}
+
+
+
+
+
+  
